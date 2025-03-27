8fc7660f8ba0d90216725d30790657fcd018eead
albertobarri <71399684+albertobarri@users.noreply.github.com>
2025-03-27 01:54:25 +0100

    Commit from Bolt to GitHub

--- src/App.tsx
+++ src/App.tsx
@@ -256,7 +256,7 @@ function App() {
 
       {/* Shopping Cart Slide-out */}
       <div
-        className={`fixed top-0 right-0 h-full w-full sm:w-96 bg-white shadow-2xl transform transition-transform duration-300 ease-in-out z-50 ${
+        className={`fixed top-0 right-0 h-full w-full md:w-96 bg-white shadow-2xl transform transition-transform duration-300 ease-in-out z-50 ${
           isCartOpen ? 'translate-x-0' : 'translate-x-full'
         }`}
       >
@@ -378,7 +378,7 @@ function App() {
       <div
         className={`fixed bottom-8 right-8 transform transition-transform duration-300 ${
           isCartAnimating ? 'scale-125' : 'scale-100'
-        } z-50`}
+        } z-50 md:hidden`}
       >
         <button
           onClick={toggleCart}
